c0afb5ddeaccf228a235d7da4609c92c91d7b6ef
Kosta Rashev <ni6to.bg@gmail.com>
2015-01-29 15:40:41 -0800

    Modify core class

--- classes/Core.php
+++ classes/Core.php
@@ -5,13 +5,48 @@
      */
     class Core {
 
+        // Public variables
+        public $objUrl;
+        public $objNavigation;
+        public $metaTitle = 'eCommerce Website';
+        public $metaDescription = 'eCommerce Website';
+        public $metaKeywords = 'eCommerce Website';
+
+        /**
+         * Constructor
+         */
+        public function __construct() {
+            $this->objUrl = new Url();
+            $this->objNavigation = new Navigation($this->objUrl);
+        }
+
         /**
          * Run
          */
         public function run() {
 
             ob_start();
-            require_once(Url::getPage());
+
+            switch ($this->objUrl->module) {
+                case 'panel':
+                    set_include_path(implode(PATH_SEPARATOR, [
+                        realpath(ROOT_PATH . DS . 'admin' . DS . TEMPLATES_DIR),
+                        realpath(ROOT_PATH . DS . 'admin' . DS . PAGES_DIR),
+                        get_include_path()
+                    ]));
+                    require_once(ROOT_PATH . DS . 'admin' . DS . PAGES_DIR .
+                        DS . $this->objUrl->currentPage . '.php');
+                    break;
+                default:
+                    set_include_path(implode(PATH_SEPARATOR, [
+                        realpath(ROOT_PATH . DS . TEMPLATES_DIR),
+                        realpath(ROOT_PATH . DS . PAGES_DIR),
+                        get_include_path()
+                    ]));
+                    require_once(ROOT_PATH . DS . PAGES_DIR .
+                        DS . $this->objUrl->currentPage . '.php');
+            }
+
             ob_get_flush();
         }
     }
