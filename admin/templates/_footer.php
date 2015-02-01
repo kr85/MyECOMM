5f3324f37eba304325a7e15729f45d8f83574914
Kosta Rashev <ni6to.bg@gmail.com>
2015-01-31 23:57:14 -0800

    Add new jquery plugins

--- admin/templates/_footer.php
+++ admin/templates/_footer.php
@@ -12,6 +12,8 @@
             </div>
         </div>
         <script src="/javascript/jquery-1.11.2.min.js" type="text/javascript"></script>
+        <script src="/javascript/jquery.livequery.js" type="text/javascript"></script>
+        <script src="/javascript/jquery.tablednd.0.7.min.js" type="text/javascript"></script>
         <script src="/javascript/basket.js" type="text/javascript"></script>
     </body>
 </html>
--- templates/_footer.php
+++ templates/_footer.php
@@ -11,6 +11,7 @@
         </div>
     </div>
     <script src="/javascript/jquery-1.11.2.min.js" type="text/javascript"></script>
+    <script src="/javascript/jquery.livequery.js" type="text/javascript"></script>
     <script src="/javascript/basket.js" type="text/javascript"></script>
 </body>
 </html>
