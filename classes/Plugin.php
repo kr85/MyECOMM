3f926e8b564e9fd589e633569b87a05f228397a5
Kosta Rashev <kosta.rashev@gmail.com>
2015-02-05 17:36:10 -0800

    Add get function to plugin class

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -1,5 +1,25 @@
 <?php
 
+    /**
+     * Class Plugin
+     */
     class Plugin {
 
+        /**
+         * Get a plugin file
+         *
+         * @param null $file
+         * @return bool|string
+         */
+        public static function get($file = null) {
+            // Create a path to the file
+            $path = PLUGIN_PATH . DS . $file . '.php';
+            // Check if the file and path exist
+            if (!empty($file) && is_file($path)) {
+                ob_start();
+                @include($path);
+                return ob_get_clean();
+            }
+            return false;
+        }
     }
