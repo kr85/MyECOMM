2a80b1df055adb37341f3e83413bb5e376cea898
Kosta Rashev <kosta.rashev@gmail.com>
2015-02-05 19:16:12 -0800

    Add get all, get all except local and get one to country class

--- classes/Country.php
+++ classes/Country.php
@@ -24,6 +24,54 @@
         }
 
         /**
+         * Get a list of all countries except the local ones
+         *
+         * @return array
+         */
+        public function getAllExceptLocal() {
+
+            $sql = "SELECT *
+                    FROM `{$this->table}`
+                    WHERE `id` != " . COUNTRY_LOCAL . "
+                    ORDER BY `name` ASC";
+
+            return $this->db->fetchAll($sql);
+        }
+
+        /**
+         * Get a list of all countries
+         *
+         * @return array
+         */
+        public function getAll() {
+
+            $sql = "SELECT *
+                    FROM `{$this->table}`
+                    ORDER BY `name` ASC";
+
+            return $this->db->fetchAll($sql);
+        }
+
+        /**
+         * Get a country by id
+         *
+         * @param null $id
+         * @return mixed|null
+         */
+        public function getOne($id = null) {
+
+            if (!empty($id)) {
+                $sql = "SELECT *
+                        FROM `{$this->table}`
+                        WHERE `id` =" . intval($id);
+
+                return $this->db->fetchOne($sql);
+            }
+
+            return null;
+        }
+
+        /**
          * Get country by id
          *
          * @param null $id
