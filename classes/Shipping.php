94bda34f53bbae9f5075aa22a9a5c47142af44f0
Kosta Rashev <kosta.rashev@gmail.com>
2015-02-18 20:06:36 -0800

    Add get shipping type by id in shipping class

--- classes/Shipping.php
+++ classes/Shipping.php
@@ -1,16 +1,41 @@
 <?php
 
+    /**
+     * Class Shipping
+     */
     class Shipping extends Application {
 
+        // Database table names
         private $tableShipping = 'shipping';
         private $tableShippingType = 'shipping_type';
         private $tableZones = 'zones';
         private $tableZonesCountryCodes = 'zones_country_codes';
-
+        // Basket instance
         public $objBasket;
 
+        /**
+         * Constructor
+         *
+         * @param null $objBasket
+         */
         public function __construct($objBasket = null) {
             parent::__construct();
             $this->objBasket = is_object($objBasket) ? $objBasket : new Basket();
+        }
+
+        /**
+         * Get a shipping type by id
+         *
+         * @param null $id
+         * @return mixed|null
+         */
+        public function getType($id = null) {
+            if (!empty($id)) {
+                $sql = "SELECT *
+                        FROM `{$this->tableShippingType}`
+                        WHERE `id` = " . intval($id);
+                return $this->db->fetchOne($sql);
+            }
+            return null;
         }
     }
