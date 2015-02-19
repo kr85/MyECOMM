d6b9366b9de51db7531937cfea206e273c901f09
Kosta Rashev <kosta.rashev@gmail.com>
2015-02-18 21:28:22 -0800

    Update admin shipping list page

--- admin/pages/shipping/list.php
+++ admin/pages/shipping/list.php
@@ -18,4 +18,34 @@
 
 <h1>Shipping Types</h1>
 
+<form method="post" class="ajax" data-action="<?php
+    echo $this->objUrl->getCurrent(['action', 'id'], false, ['action', 'add']);
+?>">
+        <table class="tbl_insert">
+            <tr>
+                <th>
+                    <label for="name" class="valid_name">
+                        Type Name:
+                    </label>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <label for="local" class="fl_r">
+                        <input type="checkbox" name="local" id="local"
+                               checked="checked"/>Local
+                    </label>
+                    <input type="text" name="name" id="name" class="fld mr_r4"/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="btn_add" class="sbm sbm_blue fl_l"> <input
+                            type="submit" id="btn_add" class="btn" value="Add"/>
+                    </label>
+                </td>
+            </tr>
+        </table>
+</form>
+
 <?php require_once('_footer.php'); ?>
