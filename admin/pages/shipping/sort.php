cc54daab2d6a48ab29a5d759f414846bffa6c04f
Kosta Rashev <kosta.rashev@gmail.com>
2015-02-19 19:44:01 -0800

    Update shipping type listing order in admin panel

---
+++ admin/pages/shipping/sort.php
@@ -0,0 +1,22 @@
+<?php
+
+    if (!empty($_POST)) {
+        $errors = [];
+
+        foreach ($_POST as $row) {
+            foreach ($row as $order => $id) {
+                $order++;
+                if (!$objShipping->updateType(['order' => $order], $id)) {
+                    $errors[] = $id;
+                }
+            }
+        }
+
+        if (empty($errors)) {
+            echo Helper::json(['error' => false]);
+        } else {
+            throw new Exception(count($errors) . ' records could not be updated.');
+        }
+    } else {
+        throw new Exception('Missing parameter.');
+    }
