6698bab8c43dd45e4e67829372aeac27fc744453
Kosta Rashev <kosta.rashev@gmail.com>
2015-02-24 19:10:16 -0800

    Refactor classes folder

---
+++ library/MyECOMM/ProjectVariable.php
@@ -0,0 +1,13 @@
+<?php
+
+    class ProjectVariable {
+
+        public static $DB_HOSTNAME        = 'localhost';
+        public static $DB_USERNAME        = 'homestead';
+        public static $DB_PASSWORD        = 'secret';
+        public static $DB_NAME            = 'myecomm';
+        public static $MAILER_USERNAME    = 'kosta.rashev@gmail.com';
+        public static $MAILER_PASSWORD    = '626685870428hate#';
+        public static $MAILER_NAME        = 'Kosta Rashev';
+        public static $PAYPAL_BUSINESS_ID = 'seller-myecomm@gmail.com';
+    }
