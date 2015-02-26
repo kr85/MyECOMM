89530b4b6d4e459a8b4fc877bcbdeb6ec163fdf1
Kosta Rashev <kosta.rashev@gmail.com>
2015-02-26 00:13:56 -0800

    Apply new mysql extension in application class

--- library/MyECOMM/Application.php
+++ library/MyECOMM/Application.php
@@ -1,18 +1,81 @@
-<?php
+<?php namespace MyECOMM;
+
+use MyECOMM\Database\Extension\MySQL;
+
+/**
+ * Class Application
+ */
+abstract class Application {
 
     /**
-     * Class Application
+     * @var MySQL instance
      */
-    class Application {
+    protected $Db;
 
-        // Database object
-        public $db;
+    /**
+     * @var Database table name
+     */
+    protected $table;
 
-        /**
-         * Constructor
-         */
-        public function __construct() {
+    /**
+     * @var Record id
+     */
+    public $id;
 
-            $this->db = new Database();
-        }
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        $this->Db = new MySQL();
     }
+
+    /**
+     * Fetch one record
+     *
+     * @param null $id
+     * @param string $field
+     * @return mixed|null
+     */
+    public function getOne($id = null, $field = 'id') {
+        return $this->Db->selectOne($this->table, $id, $field);
+    }
+
+    /**
+     * Insert a record
+     *
+     * @param null $params
+     * @return bool
+     */
+    public function insert($params = null) {
+
+        if ($this->Db->insert($this->table, $params)) {
+
+            $this->id = $this->Db->id;
+            return true;
+
+        }
+
+        return false;
+    }
+
+    /**
+     * Update a record
+     *
+     * @param null $params
+     * @param null $id
+     * @return bool|mixed
+     */
+    public function update($params = null, $id = null) {
+        return $this->Db->update($this->table, $params, $id);
+    }
+
+    /**
+     * Delete a record
+     *
+     * @param null $id
+     * @return bool|mixed
+     */
+    public function delete($id = null) {
+        return $this->Db->delete($this->table, $id);
+    }
+}
