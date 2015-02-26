0163620335d7f7c42ba111008e2a8ed7f0076821
Kosta Rashev <kosta.rashev@gmail.com>
2015-02-26 13:43:26 -0800

    Refactor upload class

--- library/MyECOMM/Upload.php
+++ library/MyECOMM/Upload.php
@@ -1,28 +1,41 @@
-<?php
+<?php namespace MyECOMM;
 
 /**
  * Class Upload
  */
 class Upload {
 
-    public $files = [];
+    /**
+     * @var array Array of upload files
+     */
+    private $files = [];
+
+    /**
+     * @var bool Overwrite file name
+     */
     public $overwrite = false;
+
+    /**
+     * @var array Array of errors
+     */
     public $errors = [];
+
+    /**
+     * @var array Array of names
+     */
     public $names = [];
 
     /**
      * Constructor
      */
     public function __construct() {
-
         $this->getUploads();
     }
 
     /**
      * Get all files to be uploaded
      */
-    public function getUploads() {
-
+    private function getUploads() {
         if (!empty($_FILES)) {
             foreach ($_FILES as $key => $value) {
                 $this->files[$key] = $value;
@@ -37,37 +50,21 @@ class Upload {
      * @return bool
      */
     public function upload($path = null) {
-
         if (!empty($path) && is_dir($path) && !empty($this->files)) {
-
             foreach ($this->files as $key => $value) {
-
                 $name = Helper::cleanString($value['name']);
-
-                if ($this->overwrite == false && is_file(
-                        $path . DS . $name
-                    )
-                ) {
+                if ($this->overwrite == false && is_file($path.DS.$name)) {
                     $prefix = date('YmdHis', time());
-                    $name = $prefix . "-" . $name;
+                    $name = $prefix."-".$name;
                 }
-
-                if (!move_uploaded_file(
-                    $value['tmp_name'],
-                    $path . DS . $name
-                )
-                ) {
+                // Move the uploaded file
+                if (!move_uploaded_file($value['tmp_name'], $path.DS.$name)) {
                     $this->errors[] = $key;
                 }
-
                 $this->names[] = $name;
             }
-
-            return empty($this->errors) ?
-                true :
-                false;
+            return (empty($this->errors)) ? true : false;
         }
-
         return false;
     }
 }
