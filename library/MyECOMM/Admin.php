687c877d669af65d52f7b9faaf2c0faac86bbde3
Kosta Rashev <kosta.rashev@gmail.com>
2015-02-26 12:29:52 -0800

    Amend admin class based on new db and namespace

--- library/MyECOMM/Admin.php
+++ library/MyECOMM/Admin.php
@@ -1,14 +1,14 @@
-<?php
+<?php namespace MyECOMM;
 
 /**
  * Class Admin
  */
 class Admin extends Application {
 
-    // The table name
-    private $table = 'admins';
-
-    public $id;
+    /**
+     * @var string The table name
+     */
+    protected $table = 'admins';
 
     /**
      * Check if it a user by email/password
@@ -18,66 +18,67 @@ class Admin extends Application {
      * @return bool
      */
     public function isUser($email = null, $password = null) {
-
-        if (!empty($email) && !empty($password)) {
-
+        if (!$this->isEmailPasswordEmpty($email, $password)) {
             $password = Login::stringToHash($password);
-
-            $sql = "SELECT * FROM `{$this->table}`
-                    WHERE `email` = '" . $this->db->escape($email) . "'
-                    AND `password` = '" . $this->db->escape(
-                    $password
-                ) . "'";
-
-            $result = $this->db->fetchOne($sql);
-
+            $sql = "SELECT *
+                    FROM `{$this->table}`
+                    WHERE `email` = ?
+                    AND `password` = ?";
+            $result = $this->Db->fetchOne($sql, [$email, $password]);
             if (!empty($result)) {
                 $this->id = $result['id'];
-
                 return true;
             }
-
             return false;
         }
-
         return false;
+    }
+
+    /**
+     * Check if email and password are empty
+     *
+     * @param null $email
+     * @param null $password
+     * @return bool
+     */
+    private function isEmailPasswordEmpty($email = null, $password = null) {
+        return (empty($email) || empty($password));
     }
 
     /**
      * Get admin's information by id
      *
      * @param null $id
-     * @return bool|mixed
+     * @return mixed|null
      */
     public function getAdmin($id = null) {
-
         if (!empty($id)) {
-            $sql = "SELECT * FROM `{$this->table}`
-                    WHERE `id` = '" . $this->db->escape($id) . "'";
-
-            return $this->db->fetchOne($sql);
+            $sql = "SELECT *
+                    FROM `{$this->table}`
+                    WHERE `id` = ?";
+            return $this->Db->fetchOne($sql, $id);
         }
-
-        return false;
+        return null;
     }
 
     /**
      * Get the full name of a registered admin
      *
      * @param null $id
-     * @return bool
+     * @return mixed|null
      */
     public function getFullNameAdmin($id = null) {
         if (!empty($id)) {
             $sql = "SELECT *,
                     CONCAT_WS(' ', `first_name`, `last_name`) AS `full_name`
                     FROM `{$this->table}`
-                    WHERE `id` = " . intval($id);
-            $result = $this->db->fetchOne($sql);
+                    WHERE `id` = ?";
+            $result = $this->Db->fetchOne($sql, $id);
             if (!empty($result)) {
                 return $result['full_name'];
             }
+            return null;
         }
-        return false;
+        return null;
     }
 }
