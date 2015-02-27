0364727cdd28bd21b65782380ab19fee8a8d97ea
Kosta Rashev <kosta.rashev@gmail.com>
2015-02-27 14:44:13 -0800

    Modify catalog pages

--- pages/catalog-item.php
+++ pages/catalog-item.php
@@ -1,55 +1,59 @@
 <?php
 
-    $id = $this->objUrl->get('item');
+use MyECOMM\Catalog;
+use MyECOMM\Basket;
+use MyECOMM\Helper;
 
-    // Check if id is empty
-    if (!empty($id)):
-        $objCatalog = new Catalog();
-        $product = $objCatalog->getProductByIdentity($id);
+$id = $this->objUrl->get('item');
 
-        // Check if product is empty
-        if (!empty($product)):
-            $this->metaTitle = $product['meta_title'];
-            $this->metaDescription = $product['meta_description'];
-            $this->metaKeywords = $product['meta_keywords'];
+// Check if id is empty
+if (!empty($id)):
+    $objCatalog = new Catalog();
+    $product = $objCatalog->getProductByIdentity($id);
 
-            // Get product's category
-            $category = $objCatalog->getCategory($product['category']);
+    // Check if product is empty
+    if (!empty($product)):
+        $this->metaTitle = $product['meta_title'];
+        $this->metaDescription = $product['meta_description'];
 
-            require_once('_header.php');
+        // Get product's category
+        $category = $objCatalog->getCategory($product['category']);
 
-            echo "<h1>Catalog :: ".$category['name']."</h1>";
+        require_once('_header.php');
 
-            $image = !empty($product['image']) ?
-                $product['image'] :
-                'unavailable.png';
+        echo "<h1>Catalog :: ".$category['name']."</h1>";
 
-            // Check if image is empty
-            if (!empty($image)):
-                $width = Helper::getImageSize(CATALOG_PATH.DS.$image, 0);
-                $width = ($width > 120) ? 120 : $width;
-                echo "<div class=\"fl_l\">";
-                echo "<div class=\"lft\"><img src=\"";
-                echo DS.CATALOG_DIR.DS.$image;
-                echo "\" alt=\"";
-                echo Helper::encodeHTML($product['name'], 1);
-                echo "\" width=\"{$width}\" /></div>";
-            endif;
+        $image = !empty($product['image']) ?
+            $product['image'] :
+            'unavailable.png';
 
-            echo "<div class=\"rgt\"><h3>" . $product['name'] . "</h3>";
-            echo "<h4><strong>" . Catalog::$currency;
-            echo $product['price'] . "</strong></h4>";
-            echo Basket::activeButton($product['id']);
-            echo "</div></div>";
-            echo "<div class=\"dev\">&#160;</div>";
-            echo "<p>" . Helper::encodeHTML($product['description']) . "</p>";
-            echo "<div class=\"dev br_td\">&#160;</div>";
-            echo "<p><a href=\"javascript:history.go(-1)\">Go back</a></p>";
-
-            require_once('_footer.php');
-        else:
-            require_once('error.php');
+        // Check if image is empty
+        if (!empty($image)):
+            $width = Helper::getImageSize(CATALOG_PATH.DS.$image, 0);
+            $width = ($width > 120) ? 120 : $width;
+            echo "<div class=\"fl_l\">";
+            echo "<div class=\"lft\"><img src=\"";
+            echo DS.CATALOG_DIR.DS.$image;
+            echo "\" alt=\"";
+            echo Helper::encodeHTML($product['name'], 1);
+            echo "\" width=\"{$width}\" /></div>";
         endif;
+
+        echo "<div class=\"rgt\"><h3>".$product['name']."</h3>";
+        echo "<h4><strong>";
+        echo $this->objCurrency->display($product['price']);
+        echo "</strong></h4>";
+        echo Basket::activeButton($product['id']);
+        echo "</div></div>";
+        echo "<div class=\"dev\">&#160;</div>";
+        echo "<p>".Helper::encodeHTML($product['description'])."</p>";
+        echo "<div class=\"dev br_td\">&#160;</div>";
+        echo "<p><a href=\"javascript:history.go(-1)\">Go back</a></p>";
+
+        require_once('_footer.php');
     else:
         require_once('error.php');
     endif;
+else:
+    require_once('error.php');
+endif;
